79eb14400f099c29a44c279c7f29f524246827ea
Maarten Jacobs <m.jacobs@defacto.nl>
2014-12-19 13:35:10 +0100

    re-annotate

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,7 @@
 #  created_at :datetime
 #  updated_at :datetime
 #  slug       :string(255)
+#  budget     :integer
 #
 
 class Project < ActiveRecord::Base
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -7,6 +7,7 @@
 #  created_at :datetime
 #  updated_at :datetime
 #  slug       :string(255)
+#  budget     :integer
 #
 
 require "spec_helper"
